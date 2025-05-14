86ebee7421ee16a0742d2a6531a40d7dcfd0afc9
senurafdo <tnsenurafernando@gmail.com>
2025-05-14 21:20:01 +0200

    delete extra examples

--- tests/lambda-test-basic-login.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('login page', async ({ page }) => {
-    await page.goto('https://www.lambdatest.com/selenium-playground/simple-form-demo');
- 
-    await page.locator("#sum1");
-    await page.locator("#sum1").fill("abc");
-    
-  });
